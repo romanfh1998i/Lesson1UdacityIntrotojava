c8d40bc5c7b4ccec75a9ec4f5ee40ac0c353cd45
Roman Franco <romanfrancoho@hotmail.com>
2020-07-15 21:52:45 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,31 @@
+class Main {
+  public static void main(String[] args) {
+    double registrar=10.0;
+    registrar=registrar+5;
+    registrar=registrar-2.5;
+    registrar=registrar+10;
+    registrar=registrar-3;
+    System.out.println(registrar);
+     int bancobalance=500;
+  bancobalance=bancobalance+250;
+  bancobalance=bancobalance-100;
+  System.out.println(bancobalance);
+  System.out.println(2.0* (5/2));
+  int dia;
+  String Mes;
+  dia=2;
+  Mes="enero";
+  System.out.println(""+Mes + "  "  +dia);
+String PrimerNombre="Roman";
+String apellido="Franco";
+String Nombre= PrimerNombre+" "+ apellido;
+System.out.println("Hola mi Nombre es "+Nombre+".");
+int Unombre=apellido.length()+Nombre.length();
+System.out.println("Hay muchas  "+Unombre+"  letras en mi Nombre");
+double fahrenheit=68.0;
+double celcius;
+celcius=(fahrenheit-32)*5/9;
+System.out.println(celcius);
+  }
+
+}
